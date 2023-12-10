ce75a1ac5e9973b63134eb2d22397cfba2fdd4ca
guanlunlu <guanlunlu1109@gmail.com>
2023-12-11 00:22:41 +0800

    state transformation (joint -> foot-end)

--- c_code/leg_kinematics.hpp
+++ c_code/leg_kinematics.hpp
@@ -1,10 +1,26 @@
+#ifndef __LEG_KINEMATICS
+#define __LEG_KINEMATICS
+
 # include <Eigen/Dense>
+# include <vector>
 
 double deg2rad(double deg);
 double rad2deg(double rad);
 double polyval(const Eigen::VectorXd& coeff, double x);
 Eigen::VectorXd polyder(const Eigen::VectorXd& coeff);
 
+/* Polynomial fitting coeff. [fk: L_OG(theta) ik: theta(L_OG)] */ 
+Eigen::VectorXd fk_pc_(8);
+Eigen::VectorXd d_fk_pc_(7);
+Eigen::VectorXd dd_fk_pc_(6);
+Eigen::VectorXd ik_pc_(8);
+
+void kinematics_setup(){
+    fk_pc_ << -0.0004362, 0.00715474, -0.04529168, 0.13936602, -0.22479562, 0.19297458, 0.01846436, 0.08280706;
+    d_fk_pc_ = polyder(fk_pc_);
+    dd_fk_pc_ = polyder(d_fk_pc_);
+    ik_pc_ << 4.10837754e+05, -6.25071622e+05, 4.02616603e+05, -1.41754291e+05, 2.93469460e+04, -3.56228405e+03, 2.44240485e+02, -7.12383946e+00;
+}
 
 Eigen::Vector2d phi2tb(const Eigen::Vector2d& phi){
     Eigen::Vector2d tb;
@@ -31,9 +47,6 @@ Eigen::Vector2d fk(const Eigen::Vector2d& tb)
     double theta = tb[0];
     double beta = tb[1];
 
-    Eigen::VectorXd fk_pc_(8);
-    fk_pc_ << -0.0004362, 0.00715474, -0.04529168, 0.13936602, -0.22479562, 0.19297458, 0.01846436, 0.08280706;
-
     double L = polyval(fk_pc_, theta);
     Eigen::Vector2d point_G;
     point_G << -L*sin(beta), -L*cos(beta);
@@ -44,9 +57,6 @@ Eigen::Vector2d ik(const Eigen::Vector2d& xy)
 {
     double x = xy[0];
     double y = xy[1];
-
-    Eigen::VectorXd ik_pc_(8);
-    ik_pc_ << 4.10837754e+05, -6.25071622e+05, 4.02616603e+05, -1.41754291e+05, 2.93469460e+04, -3.56228405e+03, 2.44240485e+02, -7.12383946e+00;
 
     double L = sqrt(pow(x, 2)+pow(y, 2));
     double theta = polyval(ik_pc_, L);
@@ -65,20 +75,43 @@ Eigen::Matrix2d jacG(const Eigen::Vector2d& tb){
     double theta = tb[0];
     double beta = tb[1];
 
-    Eigen::VectorXd fk_pc_(8);
-    fk_pc_ << -0.0004362, 0.00715474, -0.04529168, 0.13936602, -0.22479562, 0.19297458, 0.01846436, 0.08280706;
-    Eigen::VectorXd d_fk_pc(7);
-    d_fk_pc = polyder(fk_pc_);
-
-    double L_p_theta = polyval(d_fk_pc, theta);
+    double L_p_theta = polyval(d_fk_pc_, theta);
     double L = polyval(fk_pc_, theta);
 
-    Eigen::Matrix2d jacG;
-    jacG << -L_p_theta * sin(beta), -L*cos(beta),
+    Eigen::Matrix2d jG;
+    jG << -L_p_theta * sin(beta), -L*cos(beta),
             -L_p_theta * cos(beta), L*sin(beta);
-    return jacG;
+    return jG;
 }
 
+Eigen::Matrix2d djacG(const Eigen::Vector2d& tb, const Eigen::Vector2d& dtb){
+    double theta = tb[0];
+    double beta = tb[1];
+    double dtheta = dtb[0];
+    double dbeta = dtb[1];
+
+    double dj00 = -1 * (polyval(dd_fk_pc_, theta)*dtheta*sin(beta) + polyval(d_fk_pc_, theta)*cos(beta)*dbeta);
+    double dj01 = -1 * (polyval(d_fk_pc_, theta)*dtheta*cos(beta) - polyval(fk_pc_, theta)*sin(beta)*dbeta);
+    double dj10 = -1 * (polyval(dd_fk_pc_, theta)*dtheta*cos(beta) - polyval(d_fk_pc_, theta)*sin(beta)*dbeta);
+    double dj11 = (polyval(d_fk_pc_, theta)*dtheta*sin(beta) + polyval(fk_pc_, theta)*cos(beta)*dbeta);
+    Eigen::Matrix2d dj;
+    dj << dj00, dj01,
+          dj10, dj11;
+    return dj;
+}
+
+std::vector<Eigen::Vector2d> joint2footend_transform(const Eigen::Vector2d& q, const Eigen::Vector2d& q_dot){
+    std::vector<Eigen::Vector2d> footend_state;
+    Eigen::Vector2d fe_pos;
+    Eigen::Vector2d fe_vel;
+    fe_pos = fk(q);
+    fe_vel = jacG(q) * q_dot;
+    footend_state.push_back(fe_pos);
+    footend_state.push_back(fe_vel);
+    return footend_state;
+}
+
+/* Math operations */
 double polyval(const Eigen::VectorXd& coeff, double x){
     double px = 0;
     int ord = coeff.size()-1;
@@ -100,7 +133,6 @@ Eigen::VectorXd polyder(const Eigen::VectorXd& coeff){
     return d_coeff;
 }
 
-
 double deg2rad(double deg){
     return deg * M_PI /180.0;
 }
@@ -108,3 +140,5 @@ double deg2rad(double deg){
 double rad2deg(double rad){
     return rad * 180.0 / M_PI;
 }
+
+#endif
